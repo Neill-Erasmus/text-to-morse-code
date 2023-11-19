071c50316a044947d4620b8c31a1a9a5f6903875
Neill Erasmus <neill.erasmus20@gmail.com>
2023-11-19 15:54:36 +0200

    Function created to convert text to morse code.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from data import LETTERS, NUMBERS
 
+def ConvertTextList(input : str) -> list[str]:
+    output : list[str] = []
+    for _, character in enumerate(input):
+        try:
+            output.append(LETTERS[str.upper(character)])
+        except KeyError:
+            print(f'The character "{character}" is invalid!')
+    print(f"The morse code for each valid character in the sequence provided: ")
+    return output
+
 def main() -> None:
     pass
 
