d082623247553248e0cfbe7868cc9c45727d8288
Neill Erasmus <neill.erasmus20@gmail.com>
2023-11-19 16:49:30 +0200

    Added documentation.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from data import LETTERS, NUMBERS
 import sys
 
 def GetUserInput() -> str:
+    """
+    Get user input to determine whether to convert text to Morse code or exit the application.
+
+    Returns:
+        str: If 'Y' is entered, it returns the Morse code for the entered text.
+             If 'n' is entered, it exits the application.
+             If an invalid input is given, it returns 'Invalid Input!'.
+    """
+    
     user_input = input("Would you like to convert text to morse code? (Y/n): ")
     if user_input.lower() == "y":
         return ConvertTextList(user_input=input("Enter the text you would like to convert: "))
@@ -12,6 +21,16 @@ def GetUserInput() -> str:
         return "Invalid Input!"
 
 def ConvertTextList(user_input : str) -> list[str]:
+    """
+    Convert a string of text to a list of Morse code representations for each valid character.
+
+    Args:
+        user_input (str): The input text to be converted.
+
+    Returns:
+        list[str]: A list containing the Morse code representation for each valid character.
+    """
+    
     output : list[str] = []
     for _, character in enumerate(user_input):
         if character.isalpha():
@@ -22,6 +41,23 @@ def ConvertTextList(user_input : str) -> list[str]:
     return output
 
 def main() -> None:
+    """
+    Main function to run the Morse code conversion application.
+
+    This function runs a continuous loop, prompting the user for input and
+    displaying the resulting Morse code. It utilizes the GetUserInput and
+    ConvertTextList functions to handle user interactions and text conversion.
+
+    Note:
+        The loop continues until the user chooses to exit the application.
+
+    Raises:
+        SystemExit: Exits the application if the user chooses to do so.
+
+    Returns:
+        None
+    """
+    
     while True:
         print(GetUserInput())
 
